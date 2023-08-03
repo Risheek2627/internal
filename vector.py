585ff0ca39784a9b6832baadbf3672a2ab5598b1
Risheek2627 <135574671+Risheek2627@users.noreply.github.com>
2023-08-03 12:20:36 +0530

    Add files via upload
    Added new file

---
+++ vector.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import timeit
+print("Addition using vectorization = ",np.prod(np.arange(4)))
+print("time taken by vectorized product:",end="")
+print(timeit.timeit('np.sum', globals=globals()))
+total=1
+for item in range(1,4):
+    total*=item
+a=total 
+print("Addition using looping statement =",a)
+print("time taken by iterative product:",end="")
+print(timeit.timeit('a ', globals=globals()))
+
